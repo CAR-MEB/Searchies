dcbb517c301174447653e3d5ac00d87eb31a4308
xxx878 <bekatai634@gmail.com>
2024-06-30 12:59:32 +0600

    first commit

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,55 +1,94 @@
-import React, {useState, useEffect}from 'react'
-import axios from 'axios'
-import SearchItem from './SearchItem/SearchItem'
+import React from 'react';
+import { useState} from 'react';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import {man, woman} from './db.js'
+import Table from 'react-bootstrap/Table';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
 import './Search.css'
 
+function Excursion(){
+  
+  //search
 
+  const [search, setSearch] = useState('');
 
-
-const Search = () => {
-
-    const [searchies, setSearchies] = useState([])
-    const getSearchies = () => {
-        axios.get('')
-            .then((response) => {
-                setSearchies(response.data)
-            })
-    }
-    useEffect(() => {
-        getSearchies()
-    }, [])
-
-    const [value, setValue] = useState('')
-
-    const filteredClothing = searchies.filter(search => {
-        return search.name.toLowerCase().inclides(value.toLOwerCase())
-    })
-
-
-
-  return (
-    <div>
-        <div className="form">
-            <form className="search_form">
-                <input 
-                    type="text" 
-                    placeholder='Search in the clothing...'
-                    className="search_input"
-                    onChange={(event) => setValue(event.target.value)} 
-                />
-            </form>
+    return(
+        <div  className="Excursion">
+            <section>
+            <Container>
+            <Form>
+                <searchInput>
+                    <Form.Control 
+                        onChange={(e) => setSearch(e.target.value)}
+                        placeholder='Search...'
+                        style={{fontSize:"14px"}}
+                        className='search_input'
+                    />
+                </searchInput>
+        </Form>
+        <Table className='card-man'>
+          <tbody className='flex-cards'>
+            {man.filter((item) => {
+                return search.toLowerCase() === ' '
+                ? item
+                : item.name.toLowerCase().includes(search);
+            }).map((item, index) => (
+              <Card key={index} style={{ width: '279px' }}>
+      <Card.Img variant="top" src={item.img} alt="" />
+      <Card.Body>
+        <Card.Title style={{fontSize:"20px", fontWeight:"600"}}>{item.name}</Card.Title>
+        <Card.Title style={{fontSize:"18px", fontWeight:"600"}}>{item.view}</Card.Title>
+        <Card.Title style={{fontSize:"15px", fontWeight:"600"}}>{item.colors}</Card.Title>
+        <Card.Text>
+          <div className="street">
+          <p style={{fontSize:"14px"}}>{item.about}</p>
+          </div>
+          <div className="flex-rating">
+          <div className="center"><p>{item.feedback}</p>{item.feedbackText}</div>
+          </div>
+        </Card.Text>
+        <h5 style={{color:"rgb(8, 7, 7"}}><strong>{item.price}</strong></h5>
+        <Button style={{fontSize:"15px"}} variant="primary">Set</Button>
+      </Card.Body>
+    </Card>
+              ))}
+          </tbody>
+        </Table>
+        <Table className='card-woman'>
+          <tbody className='flex-cards'>
+            {woman.filter((item) => {
+                return search.toLowerCase() === ' '
+                  ? item
+                  : item.name.toLowerCase().includes(search);
+              }).map((item, index) => (
+                <Card key={index} style={{ width: '279px' }}>
+        <Card.Img variant="top" src={item.img} alt="" />
+        <Card.Body>
+          <Card.Title style={{fontSize:"20px", fontWeight:"600"}}>{item.name}</Card.Title>
+          <Card.Title style={{fontSize:"18px", fontWeight:"600"}}>{item.view}</Card.Title>
+          <Card.Title style={{fontSize:"15px", fontWeight:"600"}}>{item.colors}</Card.Title>
+          <Card.Text>
+            <div className="street">
+            <p style={{fontSize:"14px"}}>{item.about}</p>
+            </div>
+            <div className="flex-rating">
+            <div className="center"><p>{item.feedback}</p>{item.feedbackText}</div>
+            </div>
+          </Card.Text>
+          <h5 style={{color:"rgb(8, 7, 7"}}><strong>{item.price}</strong></h5>
+          <Button style={{fontSize:"15px"}} variant="primary">Отправиться</Button>
+        </Card.Body>
+      </Card>
+              ))}
+          </tbody>
+        </Table>
+      </Container>
+            </section>
+            
         </div>
-        <div className="searchies">
-            {
-                filteredClothing.map((search, index) => {
-                    return (
-                        <SearchItem search={search} key={index}/>
-                    )
-                })
-            }
-        </div>
-    </div>
-  )
+    )
 }
 
-export default Search
+export default Excursion;
